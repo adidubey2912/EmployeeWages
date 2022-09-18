b96b7692325109dbed8247ba59975b4da0e9d81a
Aditya <adi.dubey2912@gmail.com>
2022-09-18 12:55:14 +0530

    Check Employee is

---
+++ EmployeeWages/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int FullTime = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == FullTime)
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent.");
+            }
+            Console.Read();
+        }
+    }
+}
