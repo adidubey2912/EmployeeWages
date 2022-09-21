2adadb3a31f0292b93f194201177f6d0b44c5ac8
Aditya <adi.dubey2912@gmail.com>
2022-09-21 23:58:27 +0530

    Ability to compute Employee Wage for multiple companies

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -10,10 +10,7 @@ namespace EmployeeWages
     {
         public const int FullTime = 1;
         public const int PartTime = 2;
-        public const int EmpRatePerHour = 20;
-        public const int NumOfWorkingDays = 20;
-        public const int MaxHrsInMonth = 100;
-        public static int ComputeEmpWage()
+        public static int ComputeEmpWage(string Company, int EmpRatePerHour, int NumOfWorkingDays, int MaxHrsInMonth)
         {
             int EmpHrs = 0;
             int TotalEmpHrs = 0;
@@ -29,28 +26,26 @@ namespace EmployeeWages
                 {
                     case FullTime:
                         EmpHrs = 8;
-                        Console.WriteLine("Employee type is Full time.");
                         break;
                     case PartTime:
                         EmpHrs = 4;
-                        Console.WriteLine("Employee type is Part time.");
                         break;
                     default:
                         EmpHrs = 0;
-                        Console.WriteLine("Employee is absent.");
                         break;
                 }
 
                 TotalEmpHrs += EmpHrs;
-                Console.WriteLine("Working days {0} and hours {1}", TotalWorkingDays, EmpHrs);
+                Console.WriteLine("Working days {0} and hours {1}.", TotalWorkingDays, EmpHrs);
             }
             int TotalEmpWages = TotalEmpHrs * EmpRatePerHour;
-            Console.WriteLine("Monthly Employee Wage is {0}", TotalEmpWages);
+            Console.WriteLine("Total Employee Wage for company {0} is {1}.", Company, TotalEmpWages);
             return TotalEmpWages;
         }
         static void Main(string[] args)
         {
-            ComputeEmpWage();
+            ComputeEmpWage("DMart", 20, 30, 208);
+            ComputeEmpWage("Big Bazar", 15, 26, 200 );
             Console.ReadLine();
         }
     }
