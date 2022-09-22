fc99a1b55c211dc46780513b0b07bfb29cd5f28f
Aditya <adi.dubey2912@gmail.com>
2022-09-22 23:30:37 +0530

    Ability to save the Total Wage for Each Company

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -8,44 +8,14 @@ namespace EmployeeWages
 {
     public class Program
     {
-        public const int FullTime = 1;
-        public const int PartTime = 2;
-        public static int ComputeEmpWage(string Company, int EmpRatePerHour, int NumOfWorkingDays, int MaxHrsInMonth)
-        {
-            int EmpHrs = 0;
-            int TotalEmpHrs = 0;
-            int TotalWorkingDays = 0;
-
-            while(TotalEmpHrs < MaxHrsInMonth && TotalWorkingDays < NumOfWorkingDays)
-            {
-                TotalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-
-                switch (empCheck)
-                {
-                    case FullTime:
-                        EmpHrs = 8;
-                        break;
-                    case PartTime:
-                        EmpHrs = 4;
-                        break;
-                    default:
-                        EmpHrs = 0;
-                        break;
-                }
-
-                TotalEmpHrs += EmpHrs;
-                Console.WriteLine("Working days {0} and hours {1}.", TotalWorkingDays, EmpHrs);
-            }
-            int TotalEmpWages = TotalEmpHrs * EmpRatePerHour;
-            Console.WriteLine("Total Employee Wage for company {0} is {1}.", Company, TotalEmpWages);
-            return TotalEmpWages;
-        }
         static void Main(string[] args)
         {
-            ComputeEmpWage("DMart", 20, 30, 208);
-            ComputeEmpWage("Big Bazar", 15, 26, 200 );
+            EmpWageBuilderObject DMart = new EmpWageBuilderObject("DMart", 20, 10, 28);
+            EmpWageBuilderObject BigBazar = new EmpWageBuilderObject("Big Bazar", 15, 5, 20 );
+            DMart.ComputeEmpWage();
+            Console.WriteLine(DMart.toString());
+            BigBazar.ComputeEmpWage();
+            Console.WriteLine(BigBazar.toString());
             Console.ReadLine();
         }
     }
